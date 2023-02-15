5acd217cb337a14b61f08c5bbe702f4a96590fac
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2023-02-15 12:06:37 +0200

    refactor dependecy injection

--- lib/injection.dart
+++ lib/injection.dart
@@ -3,6 +3,7 @@ import 'package:grammar_app/data/datasources/quotes_local_datasource.dart';
 import 'package:grammar_app/data/repositories/quotes_repo_impl.dart';
 import 'package:grammar_app/domain/repositories/quotes_repository.dart';
 import 'package:grammar_app/domain/usecases/quotes_usecases.dart';
+import 'package:grammar_app/presentation/pages/all_themes/cubit/cubit/all_themes_cubit.dart';
 import 'package:grammar_app/presentation/pages/greeting/cubit/quotes_cubit.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,7 +12,11 @@ final sl = GetIt.I; // sl == Service Locator
 Future<void> init() async {
 // ! application Layer
   // Factory = every time a new/fresh instance of that class
-  sl.registerFactory(() => QuotesCubit(quoteUseCases: sl()));
+
+  sl.registerFactory(() => QuotesCubit(
+        quoteUseCases: sl(),
+      ));
+  sl.registerFactory(() => AllThemesCubit());
 
 // ! domain Layer
   sl.registerFactory(() => QuotesUseCases(quotesRepo: sl()));
@@ -19,8 +24,7 @@ Future<void> init() async {
 // ! data Layer
   sl.registerFactory<QuotesRepository>(
       () => QuotesRepoImpl(quotesLocalDatasource: sl()));
-  sl.registerFactory<QuotesLocalDatasource>(
-      () => QuotesLocalDatasourceImpl(client: sl()));
+  sl.registerFactory<QuotesLocalDatasource>(() => QuotesLocalDatasourceImpl());
 
 // ! externs
   sl.registerFactory(() => http.Client());
