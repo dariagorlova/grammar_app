9eb018de5dc4f3505626eaa5ca03d7fb4cbedebf
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2023-02-19 22:06:11 +0200

    refactor di

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,15 +1,16 @@
 import 'package:get_it/get_it.dart';
-import 'package:grammar_app/data/datasources/quotes_local_datasource.dart';
-import 'package:grammar_app/data/repositories/quotes_repo_impl.dart';
-import 'package:grammar_app/domain/repositories/quotes_repository.dart';
-import 'package:grammar_app/domain/usecases/quotes_usecases.dart';
-import 'package:grammar_app/presentation/pages/all_themes/cubit/cubit/all_themes_cubit.dart';
-import 'package:grammar_app/presentation/pages/greeting/cubit/quotes_cubit.dart';
-import 'package:http/http.dart' as http;
+// import 'package:grammar_app/data/datasources/quotes_local_datasource.dart';
+// import 'package:grammar_app/data/repositories/quotes_repo_impl.dart';
+// import 'package:grammar_app/domain/repositories/quotes_repository.dart';
+// import 'package:grammar_app/domain/usecases/quotes_usecases.dart';
+// import 'package:grammar_app/presentation/pages/all_themes/cubit/cubit/all_themes_cubit.dart';
+// import 'package:grammar_app/presentation/pages/greeting/cubit/quotes_cubit.dart';
+// import 'package:http/http.dart' as http;
 
 final sl = GetIt.I; // sl == Service Locator
 
 Future<void> init() async {
+  /*
 // ! application Layer
   // Factory = every time a new/fresh instance of that class
 
@@ -28,4 +29,5 @@ Future<void> init() async {
 
 // ! externs
   sl.registerFactory(() => http.Client());
+  */
 }
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,34 +1,59 @@
 import 'package:get_it/get_it.dart';
 import 'package:grammar_app/core/services/navigator.dart';
+import 'package:grammar_app/data/datasources/groups_local_datasource.dart';
 import 'package:grammar_app/data/datasources/quotes_local_datasource.dart';
+import 'package:grammar_app/data/datasources/themes_local_datasource.dart';
+import 'package:grammar_app/data/repositories/groups_repo_impl.dart';
 import 'package:grammar_app/data/repositories/quotes_repo_impl.dart';
+import 'package:grammar_app/data/repositories/themes_repo_impl.dart';
+import 'package:grammar_app/domain/repositories/groups_repository.dart';
 import 'package:grammar_app/domain/repositories/quotes_repository.dart';
+import 'package:grammar_app/domain/repositories/themes_repository.dart';
+import 'package:grammar_app/domain/usecases/groups_usecases.dart';
 import 'package:grammar_app/domain/usecases/quotes_usecases.dart';
+import 'package:grammar_app/domain/usecases/themes_usecases.dart';
 import 'package:grammar_app/navigation.dart';
 
+import 'presentation/pages/all_themes/cubit/cubit/all_themes_cubit.dart';
 import 'presentation/pages/greeting/cubit/quotes_cubit.dart';
 
 final instance = GetIt.instance;
 
 Future<void> initDI() async {
   //Logic (BLoC/Cubit)
-
   instance.registerLazySingleton(() => QuotesCubit(
         quoteUseCases: instance(),
+      ));
+  instance.registerLazySingleton(() => AllThemesCubit(
+        groupsUseCases: instance(),
+        themeUseCases: instance(),
       ));
 
   //Repository
   instance.registerLazySingleton<QuotesRepository>(() => QuotesRepoImpl(
         quotesLocalDatasource: instance(),
       ));
+  instance.registerLazySingleton<GroupsRepository>(() => GroupsRepoImpl(
+        groupsLocalDatasource: instance(),
+      ));
+  instance.registerLazySingleton<ThemesRepository>(() => ThemesRepoImpl(
+        themesLocalDatasource: instance(),
+      ));
+
   //Usecase
   instance.registerLazySingleton(() => QuotesUseCases(quotesRepo: instance()));
+  instance.registerLazySingleton(() => GroupsUseCases(groupsRepo: instance()));
+  instance.registerLazySingleton(() => ThemesUseCases(themesRepo: instance()));
 
+  // DataSource
   instance.registerLazySingleton<QuotesLocalDatasource>(
       () => QuotesLocalDatasourceImpl());
-
   //instance.registerLazySingleton<QuotesLocalDatasource>(
   //    () => QuotesRemoteDatasourceImpl(client: instance()));
+  instance.registerLazySingleton<GroupsLocalDatasource>(
+      () => GroupsLocalDatasourceImpl());
+  instance.registerLazySingleton<ThemesLocalDatasource>(
+      () => ThemesLocalDatasourceImpl());
 
   //Core
   instance.registerSingleton<AppNavigation>(NavigationImpl());
